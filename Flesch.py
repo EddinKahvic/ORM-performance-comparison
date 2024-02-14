421a9dd0eb81f8508eaf08cf949d155a95ca89d1
b21eddka <b21eddka@student.his.se>
2024-02-14 15:58:29 +0100

    Flesch formula test

---
+++ Flesch.py
@@ -0,0 +1,10 @@
+from readability import Readability
+import os
+
+with os.scandir("./docs") as entries:
+    for entry in entries:
+        with open(entry.path, encoding = 'utf-8') as f:
+            file = f.read()
+            test = Readability(file)
+            print(f'{entry.name}: {test.flesch()}')
+            f.close()
