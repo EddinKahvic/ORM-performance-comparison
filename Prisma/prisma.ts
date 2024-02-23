26e15fc484587a27b357a88991364233e97e8b9c
b21eddka <b21eddka@student.his.se>
2024-02-23 01:39:36 +0100

    Added simple & complex insert/read

--- Prisma/prisma.ts
+++ Prisma/prisma.ts
@@ -1,7 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
+// Simple insert
 export async function createSimple() {
   await prisma.pets.create({
     data: {
@@ -11,6 +14,54 @@ export async function createSimple() {
       owner_id: 2,
     },
   })
-  const allPets = await prisma.pets.findMany({})
-  console.log(allPets)
+}
+
+// Complex insert
+export async function createComplex() {
+  await prisma.pets.createMany({
+    data: [
+      {
+        name: 'Buddy',
+        birth_date: new Date('2010-01-15'),
+        type_id: 2,
+        owner_id: 4,
+      },
+      {
+        name: 'Tweety',
+        birth_date: new Date('2013-04-22'),
+        type_id: 5,
+        owner_id: 4,
+      },
+    ],
+  })
+}
+
+// Simple read
+export async function readSimple() {
+  await prisma.pets.findUnique({
+    where: {
+      id: 6,
+    },
+  })
+}
+
+// Complex read
+export async function readComplex() {
+  const testing = await prisma.pets.findMany({
+    select: {
+      name: true,
+      birth_date: true,
+      types: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    where: {
+      owners: {
+        first_name: 'Eduardo',
+        last_name: 'Rodriquez',
+      },
+    },
+  })
 }
