b1e2a7938ddda35d3bccc6aea8d303699b88d104
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-02-26 18:23:48 +0100

    added express server configuration

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-import { SomeFunction } from './MikroORM'
+import express from 'express'
+
+const app = express()
+const PORT = parseInt(process.env.PORT ?? '5000')
+app.use(express.json())
+
+// Routes
+
+app.listen(PORT, () => {
+  console.log('Server listening on port', PORT)
+})
