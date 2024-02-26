ffc922afcec090e6c7d909cfb0b9bc72fc591358
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-02-26 18:30:49 +0100

    added delete router

--- MikroORM/index.ts
+++ MikroORM/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import CreateRouter from './routes/create/create.routes'
 import ReadRouter from './routes/read/read.routes'
 import UpdateRouter from './routes/update/update.routes'
+import DeleteRouter from './routes/delete/delete.routes'
 
 export * from './mikroorm'
 export * from './generate-entities'
@@ -11,3 +12,4 @@ export const MikroORMRouter = express.Router()
 MikroORMRouter.use('/create/', CreateRouter)
 MikroORMRouter.use('/read/', ReadRouter)
 MikroORMRouter.use('/update/', UpdateRouter)
+MikroORMRouter.use('/delete/', DeleteRouter)
