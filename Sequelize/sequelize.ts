b237205e63c37ac29dce863902c257a5cd3a7d0f
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-02-27 13:49:15 +0100

    removed testconnection method

--- Sequelize/sequelize.ts
+++ Sequelize/sequelize.ts
@@ -1,12 +1,12 @@
-import { Sequelize } from "sequelize";
-import { 
-  DatabaseName, 
-  DatabaseHost, 
-  DatabasePort, 
-  DatabaseUser, 
-  DatabasePassword 
-} from "../helpers/environment";
-import { initModels} from "./Entities/init-models"
+import { Sequelize } from 'sequelize'
+import {
+  DatabaseName,
+  DatabaseHost,
+  DatabasePort,
+  DatabaseUser,
+  DatabasePassword,
+} from '../helpers/environment'
+import { initModels } from './Entities/init-models'
 
 export const sequelize = new Sequelize({
   dialect: 'mysql',
@@ -14,18 +14,8 @@ export const sequelize = new Sequelize({
   username: DatabaseUser,
   password: DatabasePassword,
   host: DatabaseHost,
-  port: DatabasePort
+  port: DatabasePort,
 })
-
-export const TestConnection  = async () =>{
-  try{
-    await sequelize.authenticate();
-    console.log("Successfully connected to database")
-  } catch(error)
-  {
-    console.log("Error connection to database: ", error)
-  }
-}
 
 export const InitializeModels = () => {
   initModels(sequelize)
