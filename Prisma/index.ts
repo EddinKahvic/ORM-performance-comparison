ecf9b6b442451ea21caec66bd62e1aaf6298f369
b21eddka <b21eddka@student.his.se>
2024-02-27 12:23:11 +0100

    housekeep

--- Prisma/index.ts
+++ Prisma/index.ts
@@ -1,1 +1,4 @@
-export * from './prisma'
+export * from './routes/create/create.routes'
+export * from './routes/delete/delete.routes'
+export * from './routes/update/update.routes'
+export * from './routes/read/read.routes'
