f764b712ed942fb853fe48d20255e71f4405dd9a
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-04-05 15:58:09 +0200

    added validation check for env variables in helper

--- helpers/environment.ts
+++ helpers/environment.ts
@@ -7,3 +7,17 @@ export const DatabaseHost = process.env.DATABASE_HOST
 export const DatabasePort = parseInt(process.env.DATABASE_PORT ?? '3306')
 export const DatabaseUser = process.env.DATABASE_USER
 export const DatabasePassword = process.env.DATABASE_PASSWORD
+
+const variablesToValidate = {
+  DatabaseName,
+  DatabaseHost,
+  DatabaseUser,
+  DatabasePassword,
+}
+
+for (let [variable, value] of Object.entries(variablesToValidate)) {
+  if (value === undefined) {
+    console.log(variable, 'is not set')
+    throw 'Environment Helper error. Have you created the required .env file?'
+  }
+}
